c46da5d3ef8e7e92b2d1a9dc0321312ce65ac54f
Rob Schoening <rob.schoening@lacework.net>
2021-12-15 06:40:34 -0800

    fix

--- terraform-aws/db.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_subnet" "db" {
-   vpc_id = aws_vpc.main.id
-   cidr_block = "10.125.3.0/24"
-
-}
--- terraform-aws/infrastructure.tf
+++ terraform-aws/infrastructure.tf
@@ -27,7 +27,7 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "db" {
    vpc_id = aws_vpc.main.id
    cidr_block = "10.125.3.0/24"
-
+    map_public_ip_on_launch = "true"
 }
 
 resource "aws_subnet" "private" {
