86226d75be43316c232b5d25a5750f336fa9a3a5
Rob Schoening <rob.schoening@lacework.net>
2021-12-15 12:49:30 -0800

    Demo change

--- terraform-aws/infrastructure.tf
+++ terraform-aws/infrastructure.tf
@@ -14,6 +14,12 @@ resource "aws_internet_gateway" "gateway" {
     }
 }
 
+resource "aws_subnet" "db" {
+   vpc_id = aws_vpc.main.id
+   cidr_block = "10.125.3.0/24"
+    map_public_ip_on_launch = "true"
+}
+
 resource "aws_subnet" "public" {
    vpc_id = aws_vpc.main.id
    cidr_block = var.vpc_public_subnet
